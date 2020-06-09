a1d1cd595d9e2a4bfe7c1867f1c0cf9d1bd2db2d
Laura Russels <39025296+LauraRussels@users.noreply.github.com>
2020-06-09 19:51:14 +0100

    plugin.yml

--- src/main/java/net/immortalrealms/Root.java
+++ src/main/java/net/immortalrealms/Root.java
@@ -1,6 +1,21 @@
 package net.immortalrealms;
 
+import net.immortalrealms.admin.commands.RankCommand;
+import net.immortalrealms.admin.tps.Lag;
+import net.immortalrealms.admin.tps.LagCheck;
+import net.immortalrealms.permissions.users.UserEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Root extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Lag(), 100L, 1L);
+
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new LagCheck(), 0L, 6000L);
+
+        getServer().getPluginManager().registerEvents(new UserEvents(), this);
+
+        getCommand("rank").setExecutor(new RankCommand());
+    }
 }
