2f10578b35afd9ffdc24c6e02618d78714865867
Laura Russels <39025296+LauraRussels@users.noreply.github.com>
2020-06-09 19:23:23 +0100

    Initial

---
+++ src/main/java/net/immortalrealms/Root.java
@@ -0,0 +1,4 @@
+package net.immortalrealms;
+
+public class Root {
+}
---
+++ src/main/java/net/immortalrealms/permissions/PermissionsHandler.java
@@ -0,0 +1,9 @@
+package net.immortalrealms.permissions;
+
+public abstract class PermissionsHandler {
+    /*
+    Group Handling
+     */
+
+
+}
