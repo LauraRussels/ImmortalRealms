635c69d38f1594d72586b02e07bb344144e497c0
Laura Russels <39025296+LauraRussels@users.noreply.github.com>
2020-06-12 15:47:49 +0100

    Player stats

---
+++ src/main/java/net/immortalrealms/players/stats/PlayerStats.java
@@ -0,0 +1,53 @@
+package net.immortalrealms.players.stats;
+
+public abstract class PlayerStats {
+    public int getPlayerKills() {
+        return playerKills;
+    }
+
+    public void setPlayerKills(int playerKills) {
+        this.playerKills = playerKills;
+    }
+
+    public int playerKills;
+
+    public int getPlayerDeaths() {
+        return playerDeaths;
+    }
+
+    public void setPlayerDeaths(int playerDeaths) {
+        this.playerDeaths = playerDeaths;
+    }
+
+    public int playerDeaths;
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    public void setPlayerScore(int playerScore) {
+        this.playerScore = playerScore;
+    }
+
+    public int playerScore;
+
+    public int getPlayerLevel() {
+        return playerLevel;
+    }
+
+    public void setPlayerLevel(int playerLevel) {
+        this.playerLevel = playerLevel;
+    }
+
+    public int playerLevel;
+
+    public double getPlayerExperience() {
+        return playerExperience;
+    }
+
+    public void setPlayerExperience(double playerExperience) {
+        this.playerExperience = playerExperience;
+    }
+
+    public double playerExperience;
+}
