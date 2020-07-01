780fd6cc6ebc0411dd73fc845915e06fcf939a4f
Laura Russels <39025296+LauraRussels@users.noreply.github.com>
2020-07-01 21:50:19 +0100

    Further push

--- Combat/src/main/java/net/immortalrealms/combat/Combat.java
+++ Combat/src/main/java/net/immortalrealms/combat/Combat.java
@@ -3,5 +3,9 @@ package net.immortalrealms.combat;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Combat extends JavaPlugin {
-
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new CombatTag(), this);
+        getServer().getPluginManager().registerEvents(new FallDamage(), this);
+    }
 }
--- Players/src/main/java/net/immortalrealms/players/Players.java
+++ Players/src/main/java/net/immortalrealms/players/Players.java
@@ -1,7 +1,16 @@
 package net.immortalrealms.players;
 
+import net.immortalrealms.players.event.PlayerDeath;
+import net.immortalrealms.players.event.PlayerJoin;
+import net.immortalrealms.players.event.PlayerQuit;
+import net.immortalrealms.utility.ScoreboardHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Players extends JavaPlugin {
-
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new PlayerDeath(new ScoreboardHandler()), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoin(new ScoreboardHandler()), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuit(), this);
+    }
 }
